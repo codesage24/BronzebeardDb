6256f407dff15eaad83edc5ef28a9afdaeb13e98
Austin Wilson <austinw@itworks.com>
2025-06-05 18:14:58 -0400

    extend tooling on map

---
+++ wwwroot/js/app.js
@@ -0,0 +1,10 @@
+﻿window.getElementRect = (element) => {
+    if (!element) return null;
+    const rect = element.getBoundingClientRect();
+    return {
+        x: rect.x,
+        y: rect.y,
+        width: rect.width,
+        height: rect.height
+    };
+};
