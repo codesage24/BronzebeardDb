7d9038f85ee891ac1e478335f573dc03b68e6917
Austin Wilson <austinwil24@gmail.com>
2025-06-09 17:38:39 -0400

    create new json maps

--- Models/Item.cs
+++ Models/Item.cs
@@ -14,7 +14,23 @@ public record Item
     string ZoneId,
     double X,
     double Y
-);
+)
+{
+    public string GetColor()
+    {
+        return Rarity switch
+        {
+            Rarity.Poor => "var(--poor-color)",
+            Rarity.Uncommon => "var(--uncommon-color)",
+            Rarity.Rare => "var(--rare-color)",
+            Rarity.Epic => "var(--epic-color)",
+            Rarity.Legendary => "var(--legendary-color)",
+            Rarity.Vanity or Rarity.Artifact => "var(--vanity-color)",
+            Rarity.Heirloom => "var(--heirloom-color)",
+            Rarity.Unknown or Rarity.Common or _ => "var(--common-color)"
+        };
+    }
+}
 
 public enum Rarity
 {
