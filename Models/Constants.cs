18a9cebc1d019f1f921ca88183304de94ad84ea6
Austin Wilson <austinw@itworks.com>
2025-06-11 17:22:22 -0400

    updates to tooltip

--- Models/Constants.cs
+++ Models/Constants.cs
@@ -8,3 +8,11 @@ public static class Constants
     public const ulong FafoGuildId = 1325200168903118938;
     public const string ApiBaseAddress = "https://sagebot.azurewebsites.net/api/";
 }
+
+public static class Position
+{
+    public const string Top = "top";
+    public const string Left = "left";
+    public const string Right = "right";
+    public const string Bottom = "bottom";
+}
